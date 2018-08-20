24a6c01fcba74951712a916238ecf61165820e83
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2018-08-20 19:02:40 +0300

    fixed indentation

--- test/unittest_airdrop.py
+++ test/unittest_airdrop.py
@@ -10,6 +10,7 @@ setup.set_verbose(False)
 setup.set_json(False)
 setup.use_keosd(False)
 
+
 class AirdropTests(unittest.TestCase):
 
     def run(self, result=None):
@@ -20,57 +21,57 @@ class AirdropTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         testnet = node.reset()
-        assert(not testnet.error)
+        assert (not testnet.error)
 
         global wallet
         wallet = eosf.Wallet()
-        assert(not wallet.error)
+        assert (not wallet.error)
 
         global account_master
         account_master = eosf.AccountMaster()
         wallet.import_key(account_master)
-        assert(not account_master.error)
+        assert (not account_master.error)
 
         global account_issuer
         account_issuer = eosf.account(account_master)
         wallet.import_key(account_issuer)
-        assert(not account_issuer.error)
+        assert (not account_issuer.error)
 
         global account_deploy_airdrop
         account_deploy_airdrop = eosf.account(account_master)
         wallet.import_key(account_deploy_airdrop)
-        assert(not account_deploy_airdrop.error)
+        assert (not account_deploy_airdrop.error)
 
         global account_deploy_token
         account_deploy_token = eosf.account(account_master)
         wallet.import_key(account_deploy_token)
-        assert(not account_deploy_token.error)
+        assert (not account_deploy_token.error)
 
         global contract_eosio_bios
         contract_eosio_bios = eosf.Contract(account_master, "eosio.bios")
-        assert(not contract_eosio_bios.error)
+        assert (not contract_eosio_bios.error)
         deployment_bios = contract_eosio_bios.deploy()
-        assert(not deployment_bios.error)
+        assert (not deployment_bios.error)
 
         global contract_token
         contract_token = eosf.Contract(account_deploy_token, "eosio.token")
-        assert(not contract_token.error)
+        assert (not contract_token.error)
         deployment_token = contract_token.deploy()
-        assert(not deployment_token.error)
-        assert(not account_deploy_token.code().error)
+        assert (not deployment_token.error)
+        assert (not account_deploy_token.code().error)
 
         global contract_airdrop
         contract_airdrop = eosf.Contract(
-                account_deploy_airdrop,
-                "eosioairdrop",
-                wast_file='/build/airdrop.wast',
-                abi_file='/build/airdrop.abi'
+            account_deploy_airdrop,
+            "eosioairdrop",
+            wast_file='/build/airdrop.wast',
+            abi_file='/build/airdrop.abi'
         )
-        assert(not contract_airdrop.error)
+        assert (not contract_airdrop.error)
 
         deployment_airdrop = contract_airdrop.deploy()
-        assert(not deployment_airdrop.error)
-        assert(not account_deploy_airdrop.code().error)
+        assert (not deployment_airdrop.error)
+        assert (not account_deploy_airdrop.code().error)
 
     def setUp(self):
         pass
@@ -83,11 +84,11 @@ class AirdropTests(unittest.TestCase):
             "maximum_supply": "1000000000.0000 TST",
         }
 
-        assert(not contract_token.push_action(
-                "create",
-                json.dumps(createTokenActionJSON),
-                account_deploy_token)
-                    .error)
+        assert (not contract_token.push_action(
+            "create",
+            json.dumps(createTokenActionJSON),
+            account_deploy_token)
+                .error)
 
         cprint(""" 2. Action `create` should be executed at airdrop contract """, 'green')
         createAirdropActionJSON = {
@@ -96,24 +97,24 @@ class AirdropTests(unittest.TestCase):
             "asset": "0.0000 TST"
         }
 
-        assert(not contract_airdrop.push_action(
-                "create",
-                json.dumps(createAirdropActionJSON),
-                account_deploy_airdrop)
-                    .error)
+        assert (not contract_airdrop.push_action(
+            "create",
+            json.dumps(createAirdropActionJSON),
+            account_deploy_airdrop)
+                .error)
 
         cprint(""" 3. Token holder should issue currency to airdrop contract """, 'green')
 
         issueTokenActionJSON = {
             "to": str(account_deploy_airdrop),
             "quantity": "100.0000 TST",
-            "memo":"memo"
+            "memo": "memo"
         }
-        assert(not contract_token.push_action(
-                "issue",
-                json.dumps(issueTokenActionJSON),
-                account_issuer)
-                    .error)
+        assert (not contract_token.push_action(
+            "issue",
+            json.dumps(issueTokenActionJSON),
+            account_issuer)
+                .error)
 
     def test_02(self):
         cprint(""" Tests for calling methods """, "red")
@@ -122,33 +123,33 @@ class AirdropTests(unittest.TestCase):
         airdrop_account_pubkey = account_deploy_airdrop.json["permissions"][1]["required_auth"]["keys"][0]["key"]
 
         permissionActionJSON = {
-            "account":str(account_deploy_airdrop),
-            "permission":"active",
-            "parent":"owner",
-            "auth":{
-                "threshold":1,
-                "keys":[
+            "account": str(account_deploy_airdrop),
+            "permission": "active",
+            "parent": "owner",
+            "auth": {
+                "threshold": 1,
+                "keys": [
                     {
-                        "key":str(airdrop_account_pubkey),
-                        "weight":1
+                        "key": str(airdrop_account_pubkey),
+                        "weight": 1
                     }
                 ],
-                "accounts":[
+                "accounts": [
                     {
-                        "permission":{
-                            "actor":str(account_deploy_airdrop),
-                            "permission":"eosio.code"
+                        "permission": {
+                            "actor": str(account_deploy_airdrop),
+                            "permission": "eosio.code"
                         },
-                        "weight":1
+                        "weight": 1
                     }
                 ],
-                "waits":[]
+                "waits": []
             }
         }
 
         setPermissionAction = json.dumps(permissionActionJSON)
 
-        assert(not contract_eosio_bios.push_action("updateauth", setPermissionAction, account_deploy_airdrop).error)
+        assert (not contract_eosio_bios.push_action("updateauth", setPermissionAction, account_deploy_airdrop).error)
 
         cprint(""" 5. Action `drop` should transfer tokens  """, 'green')
         global airdrop
@@ -166,35 +167,36 @@ class AirdropTests(unittest.TestCase):
         for x in range(len(airdrop["address"])):
             receiver = eosf.account(account_master, airdrop["address"][x])
             wallet.import_key(receiver)
-            assert(not receiver.error)
+            assert (not receiver.error)
 
         dropAirdropAction = '{"token_contract":' + str(account_deploy_token) + \
                             ', "asset": "0.0000 TST"' + \
                             ', "addresses": [' + airAddresses + \
                             '], "amounts": [' + airAmounts + ']}'
 
-        assert(not contract_airdrop.push_action("drop", dropAirdropAction, account_issuer, output=True).error)
+        assert (not contract_airdrop.push_action("drop", dropAirdropAction, account_issuer, output=True).error)
 
     def test_03(self):
         cprint(""" Tests for checking values """, "red")
-        cprint(""" 6. Issuer should withdraaw tokens from airdrop contract """, 'green')
+        cprint(""" 6. Issuer should withdraw tokens from airdrop contract """, 'green')
 
         withdrawValue = "1.0000 TST"
         withdrawAirdropAction = '{"token_contract":' + str(account_deploy_token) + \
                                 ', "value": "' + withdrawValue + '"}'
 
-        assert(not contract_airdrop.push_action("withdraw",
-                                                withdrawAirdropAction,
-                                                account_issuer).error)
+        assert (not contract_airdrop.push_action("withdraw",
+                                                 withdrawAirdropAction,
+                                                 account_issuer).error)
 
         balanceIssuer = contract_token.table("accounts", account_issuer.name).json["rows"][0]["balance"][:-4]
-        assert(balanceIssuer == withdrawValue[:-4])
+        assert (balanceIssuer == withdrawValue[:-4])
 
         cprint(""" 7. Transferred tokens should exist on accounts """, 'green')
         decimals = 10 ** 4
 
         for account in range(len(airdrop["address"])):
-            balance = float(contract_token.table("accounts", airdrop["address"][account]).json["rows"][0]["balance"][:-4])
+            balance = float(
+                contract_token.table("accounts", airdrop["address"][account]).json["rows"][0]["balance"][:-4])
             expectedBalance = int(airdrop["amount"][account]) / decimals
             assert (balance == expectedBalance)
 
@@ -204,6 +206,7 @@ class AirdropTests(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         node.stop()
+
 
 if __name__ == "__main__":
     unittest.main()
