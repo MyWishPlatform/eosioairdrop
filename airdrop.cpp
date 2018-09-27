3a0f377235263ac6fc7d58d5ecfd2dae56422978
kirillsurkov <surkov.k@mywish.io>
2018-09-27 11:59:53 +0200

    composite index

--- airdrop.cpp
+++ airdrop.cpp
@@ -2,20 +2,24 @@
 
 void airdrop::create(account_name issuer, account_name token_contract, eosio::symbol_type symbol) {
 	require_auth(this->_self);
-	drop_index drops(this->_self, symbol.name());
-	eosio_assert(drops.find(PK(issuer, token_contract)) == drops.end(), "Airdrop for this contract and symbol already exists");
+	drop_index drops(this->_self, issuer);
+	auto composite_index = drops.get_index<N(contractsymb)>();
+	eosio_assert(composite_index.find(PK(token_contract, symbol)) == composite_index.end(), "Airdrop already exists");
 	drops.emplace(this->_self, [&](auto& d) {
-		d.issuer = issuer;
-		d.token_contract = token_contract;
+		d.pk = drops.available_primary_key();
+		d.contract = token_contract;
+		d.symbol = symbol;
 	});
 }
 
 void airdrop::drop(account_name issuer, account_name token_contract, eosio::symbol_type symbol, eosio::vector<account_name> addresses, eosio::vector<int64_t> amounts) {
 	require_auth(issuer);
 
-	drop_index drops(this->_self, symbol.name());
-	auto record = drops.find(PK(issuer, token_contract));
-	eosio_assert(record != drops.end(), "Airdrop not registered");
+	drop_index drops(this->_self, issuer);
+	auto composite_index = drops.get_index<N(contractsymb)>();
+
+	auto record = composite_index.find(PK(token_contract, symbol));
+	eosio_assert(record != composite_index.end(), "Airdrop not registered");
 
 	eosio_assert(addresses.size() == amounts.size(), "Lengths not match");
 	eosio::extended_asset value(eosio::asset(0, symbol), token_contract);
@@ -28,9 +32,11 @@ void airdrop::drop(account_name issuer, account_name token_contract, eosio::symb
 void airdrop::withdraw(account_name issuer, account_name token_contract, eosio::asset value) {
 	require_auth(issuer);
 
-	drop_index drops(this->_self, value.symbol.name());
-	auto record = drops.find(PK(issuer, token_contract));
-	eosio_assert(record != drops.end(), "Airdrop not registered");
+	drop_index drops(this->_self, issuer);
+	auto composite_index = drops.get_index<N(contractsymb)>();
+
+	auto record = composite_index.find(PK(token_contract, value.symbol));
+	eosio_assert(record != composite_index.end(), "Airdrop not registered");
 
 	eosio::currency::inline_transfer(this->_self, issuer, eosio::extended_asset(value, token_contract), "withdraw");
 }
--- airdrop.hpp
+++ airdrop.hpp
@@ -4,17 +4,22 @@
 #include <eosiolib/vector.hpp>
 #include <eosiolib/eosio.hpp>
 
-#define PK(ISSUER, TOKEN) ((static_cast<int128_t>(ISSUER) << 64) + (TOKEN))
+#define PK(contract, symbol) ((static_cast<uint128_t>(contract) << 64) + (symbol.value))
 
 class airdrop : public eosio::contract {
 private:
 	struct drop {
-		account_name issuer;
-		account_name token_contract;
-		uint128_t primary_key() const { return PK(issuer, token_contract); }
+		uint64_t pk;
+		account_name contract;
+		eosio::symbol_type symbol;
+
+		uint64_t primary_key() const { return pk; }
+		uint128_t get_contract_symbol() const { return PK(this->contract, this->symbol); }
+
+		EOSLIB_SERIALIZE(drop, (pk)(contract)(symbol))
 	};
 
-	typedef eosio::multi_index<N(drop), drop> drop_index;
+	typedef eosio::multi_index<N(drop), drop, eosio::indexed_by<N(contractsymb), eosio::const_mem_fun<drop, uint128_t, &drop::get_contract_symbol>>> drop_index;
 
 public:
 	airdrop(account_name self) :
